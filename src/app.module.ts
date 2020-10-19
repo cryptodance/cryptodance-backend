92448f292baf221a6d49ae70d2bd0182e11f2faa
iganbold <itgel.ganbold1@gmail.com>
2020-10-18 22:14:37 -0700

    Add orderbooks, config, redis, schedule modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderbooksModule } from './orderbooks/orderbooks.module';
+import { RedisCacheModule } from './redis-cache/redis-cache.module';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    OrderbooksModule,
+    RedisCacheModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

c37d62b5ec0188bebc787fd002588de563f92ad9
iganbold <itgel.ganbold1@gmail.com>
2020-10-18 22:08:32 -0700

    Create Redis cache module

---
+++ src/redis-cache/redis-cache.module.ts
@@ -0,0 +1,22 @@
+import { Module, CacheModule } from '@nestjs/common';
+import { RedisCacheService } from './redis-cache.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: configService.get('REDIS_TTL'),
+      }),
+    }),
+  ],
+  providers: [RedisCacheService],
+  exports: [RedisCacheService],
+})
+export class RedisCacheModule {}

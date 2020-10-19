f4f30a5a12939ad0b47756f7bc88507392361ea2
iganbold <itgel.ganbold1@gmail.com>
2020-10-18 22:15:30 -0700

    Add helmet and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // app.setGlobalPrefix('api/v1');
+  app.use(helmet());
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
